ac43709ae3606a646ae1e86692f7e939398a9163
Fajar-09 <fajarrh2002@gmail.com>
2022-09-18 16:47:04 +0700

    feat: ini adalah commit-an pertamaku

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Helo, Dunia");
+
+//kita harus kasih tau ke git file apa yang ingin kita track perubahannya
+//git add <nama file> -> track di file yg dipilih
+//git add . -> track semua file
+
+//kita ingin lihat apa saja yg berubah
+//git status
+
